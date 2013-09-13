80346253487f9fbe5f65edf3219d0ca4bceb7a48
John Thomas <jthomas@kditek.com>
2013-09-13 16:52:09 -0400

    Added Facebook code

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template,url_for
+from flask import request, session, redirect
+from flask_oauth import OAuth
+
 #----------------------------------------
 # initialization
 #----------------------------------------
@@ -11,12 +14,93 @@ app.config.update(
 )
 
 #----------------------------------------
+# facebook authentication
+#----------------------------------------
+
+FACEBOOK_APP_ID = os.getenv('FACEBOOK_APP_ID')
+FACEBOOK_APP_SECRET = os.getenv('FACEBOOK_APP_SECRET')
+app.secret_key = os.urandom(24)
+
+print '-----------------------------------'
+print FACEBOOK_APP_ID
+print FACEBOOK_APP_SECRET
+print '-----------------------------------'
+
+
+oauth = OAuth()
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('email, ')}
+)
+
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_token')
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('facebook_token', None)
+
+
+@app.route("/facebook_login")
+def facebook_login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next'), _external=True))
+
+@app.route("/facebook_authorized")
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    #if resp is None or 'access_token' not in resp:
+    #    return redirect(next_url)
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['logged_in'] = True
+    session['facebook_token'] = (resp['access_token'], '')
+
+    data = facebook.get('/me').data
+    if 'id' in data and 'name' in data:
+        user_id = data['id']
+        user_name = data['name']
+        print user_id
+        print user_name
+
+    return redirect(next_url)
+
+@app.route("/logout")
+def logout():
+
+
+    fb_logout= "https://www.facebook.com/logout.php?next=" + url_for('index') + "&access_token=" + get_facebook_token()[0]
+
+    pop_login_session()
+    print fb_logout
+    return redirect(fb_logout)
+
+#----------------------------------------
 # controllers
 #----------------------------------------
 
+@app.route('/favicon.ico')
+def favicon():
+    return url_for('static',filename='ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def index():
+    return render_template('index.html')
 
 #----------------------------------------
 # launch
